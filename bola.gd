4459b47ebc76d1518942214d9e15eb312e02750f
Rick Willian <rick_willian@hotmail.com>
2025-01-27 22:03:25 -0300

    Adicionando como exemplo em sala

---
+++ bola.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var velocy: int = 100
+var velocx: int = 100
+var diry: int = 1
+var dirx: int = 1
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	position.y = position.y + diry * (velocy * delta)
+	position.x = position.x + dirx * (velocx * delta)
+	#print(position)
+	if position.y >= 640:
+		diry = -diry
+		#position.y = 640
+	elif position.y <= 10:
+		diry = - diry
